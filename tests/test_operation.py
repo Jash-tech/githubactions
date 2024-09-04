e7e63c4634b4993ccee10bae57ab07abc8236293
Jash-tech <jashsuke@gmail.com>
2024-09-04 12:54:17 +0530

    math and test operation created

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operation import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(3,3)==6
+    assert add(7,3)==10
+
+def test_sub():
+    assert sub(2,3)==-1
+    assert add(3,3)==0
+    assert add(7,3)==4
